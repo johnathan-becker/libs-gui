4e934d38e95738b912971002b1657cfbac4ba2c8
Johnathan Becker <john.becker@keysight.com>
2024-10-10 11:42:07 -0600

    try this

--- Tests/gui/GSCodingFlags/GSCellFlags.m
+++ Tests/gui/GSCodingFlags/GSCellFlags.m
@@ -29,53 +29,58 @@ char* print_binary(uint32_t value) {
 
 int main()
 {
-  CREATE_AUTORELEASE_POOL(arp);
-  GSCellFlagsUnion mask = { { 0 } };
+    CREATE_AUTORELEASE_POOL(arp);
+    GSCellFlagsUnion mask = { { 0 } };
 
-  START_SET("GSCodingFlags GNUstep CellFlags Union")
-  
-  NS_DURING
-  {
-    [NSApplication sharedApplication];
-  }
-  NS_HANDLER
-  {
-    if ([[localException name] isEqualToString: NSInternalInconsistencyException ])
-       SKIP("It looks like GNUstep backend is not yet installed")
-  }
-  NS_ENDHANDLER
-  // first make sure flags translate to values
-  mask.flags.state = 1;
-  mask.flags.highlighted = 1;
-  mask.flags.disabled = 0;
-  mask.flags.editable = 1;
+    START_SET("GSCodingFlags GNUstep CellFlags Union")
+    
+    NS_DURING
+    {
+      [NSApplication sharedApplication];
+    }
+    NS_HANDLER
+    {
+      if ([[localException name] isEqualToString: NSInternalInconsistencyException ])
+        SKIP("It looks like GNUstep backend is not yet installed")
+    }
+    NS_ENDHANDLER
+    // first make sure flags translate to values
+    mask.flags.state = 1;
+    mask.flags.selectable = 1;
+    mask.flags.scrollable = 1;
+    mask.flags.editable = 1;
+    mask.flags.continuous = 1;
+    mask.flags.useUserKeyEquivalent = 1;
+    mask.flags.truncateLastLine = 1;
 
-  char * mask_Value = print_binary(mask.value);
+    char * mask_Value = print_binary(mask.value);
 
 #if GS_WORDS_BIGENDIAN == 1
-  pass(mask.value == 0b00000000000000000000000000001101, "mask.flags translates to mask.value: %s", mask_Value);
+    pass(mask.value == 0b00000001001000000000110100000001, "mask.flags translates to mask.value");
 #else
-  pass(mask.value == 0b10000011000000000000000000000000, "mask.flags translates to mask.value");
+    pass(mask.value == 0b00000001001000000000110100000001, "mask.flags translates to mask.value");
 #endif
-// reset mask
-mask.value = 0;
-mask.flags = (GSCellFlags){0};
-// now make sure values translate to flags
+    // reset mask
+    mask.value = 0;
+    mask.flags = (GSCellFlags){0};
+    // now make sure values translate to flags
 #if GS_WORDS_BIGENDIAN == 1
-  mask.value = 0b00000000000000000000000000001101;
+    mask.value = 0b00000001001000000000110100000001;
 #else
-  mask.value = 0b10000011000000000000000000000000;
+    mask.value = 0b00000001001000000000110100000001;
 #endif
 
-char * mask_Value2 = print_binary(mask.value);
+    char * mask_Value2 = print_binary(mask.value);
+    pass(mask.flags.state = 1, "state is correctly set: %s", mask_Value2);
+    pass(mask.flags.selectable = 1, "selectable is correctly set: %s", mask_Value2);
+    pass(mask.flags.scrollable = 1, "scrollable is correctly set: %s", mask_Value2);
+    pass(mask.flags.editable = 1, "editable is correctly set: %s", mask_Value2);
+    pass(mask.flags.continuous = 1, "continuous is correctly set: %s", mask_Value2);
+    pass(mask.flags.useUserKeyEquivalent = 1, "useUserKeyEquivalent is correctly set: %s", mask_Value2);
+    pass(mask.flags.truncateLastLine = 1, "truncateLastLine is correctly set: %s", mask_Value2);
+    
+    END_SET("GSCodingFlags GNUstep CellFlags Union")
 
-pass(mask.flags.state == 1, "state is correctly set: %s", mask_Value2);
-pass(mask.flags.highlighted == 1, "highlighted is correctly set: %s", mask_Value2);
-pass(mask.flags.disabled == 0, "disabled is correctly set: %s", mask_Value2);
-pass(mask.flags.editable == 1, "editable is correctly set: %s", mask_Value2);
- 
-END_SET("GSCodingFlags GNUstep CellFlags Union")
-
-DESTROY(arp);
-return 0;
+    DESTROY(arp);
+    return 0;
 }
