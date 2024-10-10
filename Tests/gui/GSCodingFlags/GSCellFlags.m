f7098212365c1b011cf082781d9618f77260cd1d
Johnathan Becker <john.becker@keysight.com>
2024-10-10 11:23:42 -0600

    ok

--- Tests/gui/GSCodingFlags/GSCellFlags.m
+++ Tests/gui/GSCodingFlags/GSCellFlags.m
@@ -53,18 +53,18 @@ int main()
   char * mask_Value = print_binary(mask.value);
 
 #if GS_WORDS_BIGENDIAN == 1
-  pass(mask.value == 0b10000011000000000000000000000000, "mask.flags translates to mask.value");
-#else
   pass(mask.value == 0b00000000000000000000000000001101, "mask.flags translates to mask.value: %s", mask_Value);
+#else
+  pass(mask.value == 0b10000011000000000000000000000000, "mask.flags translates to mask.value");
 #endif
 // reset mask
 mask.value = 0;
 mask.flags = (GSCellFlags){0};
 // now make sure values translate to flags
 #if GS_WORDS_BIGENDIAN == 1
-  mask.value = 0b10000011000000000000000000000000;
-#else
   mask.value = 0b00000000000000000000000000001101;
+#else
+  mask.value = 0b10000011000000000000000000000000;
 #endif
 
 char * mask_Value2 = print_binary(mask.value);
